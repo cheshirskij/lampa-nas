0124f8aa3aad56a8985035554c7c1a5975cf2429
cheshirskij <116010691+cheshirskij@users.noreply.github.com>
2026-03-17 21:56:59 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,58 @@
-(function () {
+(function() {
     'use strict';
 
-    // 1. Создаем саму страницу, которая откроется при клике
-    Lampa.Component.add('jelly_nas_page', function (object) {
-        var comp = new Lampa.InteractionMain(object);
-
-        comp.create = function () {
-            this.activity.loader(true); // Рисуем крутилку загрузки
-            this.build([]);
-        };
-
-        comp.build = function (items) {
-            var _this = this;
-            this.activity.loader(false); // Убираем загрузку
-
-            // Покажем красивую заглушку
-            var empty = Lampa.Template.get('empty', {
-                title: 'Jellyfin NAS',
-                descr: 'Связь через GitHub Pages работает! Скоро здесь будет твой Proxmox.'
-            });
-
-            this.display(empty);
-        };
-
-        return comp;
+    // 1. Добавляем перевод названия (чтобы было красиво на разных языках)
+    Lampa.Lang.add({
+        my_nas_title: {
+            ru: "Мой Jellyfin NAS",
+            en: "My Jellyfin NAS"
+        }
     });
 
-    // 2. Регистрируем плагин в системе
-    function StartPlugin() {
-        Lampa.Plugins.add({
-            name: 'Jellyfin NAS',
-            description: 'Локальный сервер через GitHub Pages',
-            auth: false,
-            // Иконка в виде сервера
-            icon: '<svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 13H4V11H20V13ZM20 17H4V15H20V17ZM20 21H4V19H20V21ZM10 3L12 5L14 3H20C21.1 3 22 3.9 22 5V21C22 22.1 21.1 23 20 23H4C2
+    // 2. Функция для вставки кнопки в меню настроек
+    function addNasSettings() {
+        // Проверяем, не добавлена ли уже кнопка, чтобы не дублировать
+        if (Lampa.Settings.main && !Lampa.Settings.main().render().find('[data-component="my_nas_settings"]').length) {
+            
+            // Генерируем HTML код кнопки (используем иконку облака/сервера)
+            var field = $('<div class="settings-folder selector" data-component="my_nas_settings">' +
+                '<div class="settings-folder__icon">' +
+                    '<svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 13H5V11H19V13ZM19 17H4V15H19V17ZM19 21H5V19H19V21ZM5 5V7H19V5H5Z" fill="white"/></svg>' +
+                '</div>' +
+                '<div class="settings-folder__name">' + Lampa.Lang.translate('my_nas_title') + '</div>' +
+            '</div>');
+
+            // Вставляем кнопку после раздела "Остальное" (more)
+            Lampa.Settings.main().render().find('[data-component="more"]').after(field);
+            
+            // Обновляем навигацию, чтобы кнопка стала кликабельной
+            Lampa.Settings.main().update();
+        }
+    }
+
+    // 3. Вешаем действие при нажатии на кнопку
+    Lampa.Settings.listener.follow('open', function(e) {
+        if (e.name == 'main') {
+            e.body.find('[data-component="my_nas_settings"]').on('hover:enter', function() {
+                // Пока просто выводим уведомление, позже заменим на открытие списка файлов
+                Lampa.Noty.show('Подключение к Jellyfin на Proxmox...');
+                
+                // Здесь можно вызвать открытие твоей страницы
+                Lampa.Activity.push({
+                    url: '',
+                    title: 'Jellyfin NAS',
+                    component: 'jelly_nas_page', // Эту страницу мы создавали в прошлый раз
+                    page: 1
+                });
+            });
+        }
+    });
+
+    // Запуск при готовности приложения
+    if (window.appready) addNasSettings();
+    else {
+        Lampa.Listener.follow('app', function(e) {
+            if (e.type == 'ready') addNasSettings();
+        });
+    }
+})();
