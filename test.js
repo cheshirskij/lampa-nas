93d152d27f83fa6b4aa1d66929275f128db307b7
cheshirskij <116010691+cheshirskij@users.noreply.github.com>
2026-03-18 21:07:46 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,10 +4,10 @@
     var nas_host = 'http://178.234.15.238:8096';
     var nas_key  = 'b4659bb0cc0c476bb7bf3113fef553f9';
     
-    // SVG иконка Jellyfin
+    // Иконка Jellyfin (SVG)
     var icon = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM11.05 17.5L6.15 12.6L7.56 11.19L11.05 14.68L16.44 9.29L17.85 10.7L11.05 17.5Z" fill="#AA5CC3"/></svg>';
 
-    // Функция получения данных (PlaybackInfo для субтитров + поток)
+    // Функция проигрывания с сабами
     function playFile(item) {
         Lampa.Noty.show('Загрузка субтитров...');
         var network = new Lampa.Reguest();
@@ -33,11 +33,10 @@
         });
     }
 
-    // Главное меню выбора (Select)
+    // Меню выбора (Select)
     function openJellyMenu(query) {
         var network = new Lampa.Reguest();
-        var url = nas_host + '/Items?api_key=' + nas_key + '&Recursive=true&IncludeItemTypes=Movie,Episode,Video&Limit=50';
-        
+        var url = nas_host + '/Items?api_key=' + nas_key + '&Recursive=true&IncludeItemTypes=Movie,Episode,Video&Limit=80';
         if (query) url += '&searchTerm=' + encodeURIComponent(query.replace(/[^a-zA-Zа-яА-Я0-9\s]/g, ''));
         else url += '&SortBy=DateCreated&SortOrder=Descending';
 
@@ -55,30 +54,26 @@
         });
     }
 
+    // Внедрение кнопки в настройки (как в store.js)
+    Lampa.Listener.follow('activity', function (e) {
+        if (e.type == 'render' && e.component == 'settings') {
+            var item = $('<div class="settings-folder selector" data-static="true">' +
+                '<div class="settings-folder__icon">' + icon + '</div>' +
+                '<div class="settings-folder__name">Jellyfin NAS</div>' +
+                '<div class="settings-folder__descr">Ваша медиатека и субтитры</div>' +
+            '</div>');
+
+            item.on('hover:enter', function () {
+                openJellyMenu(false);
+            });
+
+            // Вставляем в список настроек
+            e.render.find('.settings-list').append(item);
+        }
+    });
+
+    // Кнопка в карточке фильма
     function startPlugin() {
-        // --- ВНЕДРЕНИЕ В НАСТРОЙКИ ---
-        // Создаем кнопку для главного меню настроек
-        var settings_item = $('<div class="settings-folder selector" data-component="jellyfin_nas">' +
-            '<div class="settings-folder__icon">' + icon + '</div>' +
-            '<div class="settings-folder__name">Jellyfin NAS</div>' +
-            '<div class="settings-folder__descr">Ваша медиатека с субтитрами</div>' +
-        '</div>');
-
-        settings_item.on('hover:enter', function () {
-            openJellyMenu(false);
-        });
-
-        // Слушаем открытие настроек и втыкаем кнопку
-        Lampa.Listener.follow('activity', function (e) {
-            if (e.type == 'render' && e.component == 'settings') {
-                var container = e.render.find('.settings-list');
-                if (!container.find('[data-component="jellyfin_nas"]').length) {
-                    container.append(settings_item);
-                }
-            }
-        });
-
-        // --- КНОПКА В КАРТОЧКЕ ---
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
                 var btn = $('<div class="full-start__button selector view--online jelly-nas-btn">' + icon + '<span>Jellyfin</span></div>');
