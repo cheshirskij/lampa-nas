4834e72c46fdc6b7be38ad1ba238858dd6e7a454
cheshirskij <116010691+cheshirskij@users.noreply.github.com>
2026-03-18 21:02:39 +0300

    Update test.js

--- test.js
+++ test.js
@@ -3,28 +3,13 @@
 
     var nas_host = 'http://178.234.15.238:8096';
     var nas_key  = 'b4659bb0cc0c476bb7bf3113fef553f9';
-    var icon = '<svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM11.05 17.5L6.15 12.6L7.56 11.19L11.05 14.68L16.44 9.29L17.85 10.7L11.05 17.5Z" fill="#AA5CC3"/></svg>';
+    
+    // SVG иконка Jellyfin
+    var icon = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM11.05 17.5L6.15 12.6L7.56 11.19L11.05 14.68L16.44 9.29L17.85 10.7L11.05 17.5Z" fill="#AA5CC3"/></svg>';
 
-    // Функция поиска/получения данных
-    function getJellyData(query, callback) {
-        var network = new Lampa.Reguest();
-        var url = nas_host + '/Items?api_key=' + nas_key + '&Recursive=true&IncludeItemTypes=Movie,Episode,Video&Limit=50';
-        
-        if(query) {
-            var cleanTitle = query.replace(/[^a-zA-Zа-яА-Я0-9\s]/g, '');
-            url += '&searchTerm=' + encodeURIComponent(cleanTitle);
-        } else {
-            url += '&SortBy=DateCreated&SortOrder=Descending';
-        }
-
-        network.silent(url, callback, function () {
-            Lampa.Noty.show('Jellyfin: Ошибка связи');
-            callback({Items: []});
-        });
-    }
-
-    // Логика запуска видео с субтитрами
-    function playWithSubs(item) {
+    // Функция получения данных (PlaybackInfo для субтитров + поток)
+    function playFile(item) {
+        Lampa.Noty.show('Загрузка субтитров...');
         var network = new Lampa.Reguest();
         network.silent(nas_host + '/Items/' + item.Id + '/PlaybackInfo?api_key=' + nas_key, function(data) {
             var subs = [];
@@ -33,7 +18,7 @@
                     if (stream.Type === 'Subtitle') {
                         subs.push({
                             label: stream.DisplayTitle || stream.Language || 'Субтитры',
-                            url: nas_host + '/Videos/' + item.Id + '/' + data.MediaSources[0].Id + '/Subtitles/' + stream.Index + '/0/Stream.' + (stream.Codec || 'vtt') + '?api_key=' + nas_key,
+                            url: nas_host + '/Videos/' + item.Id + '/' + data.MediaSources[0].Id + '/Subtitles/' + stream.Index + '/0/Stream.' + (stream.Codec || 'vtt') + '?api_key=' + nas_key
                         });
                     }
                 });
@@ -48,49 +33,64 @@
         });
     }
 
-    // Вызов меню выбора
-    function showJellyMenu(query) {
-        getJellyData(query, function(data) {
+    // Главное меню выбора (Select)
+    function openJellyMenu(query) {
+        var network = new Lampa.Reguest();
+        var url = nas_host + '/Items?api_key=' + nas_key + '&Recursive=true&IncludeItemTypes=Movie,Episode,Video&Limit=50';
+        
+        if (query) url += '&searchTerm=' + encodeURIComponent(query.replace(/[^a-zA-Zа-яА-Я0-9\s]/g, ''));
+        else url += '&SortBy=DateCreated&SortOrder=Descending';
+
+        network.silent(url, function (data) {
             if (data.Items && data.Items.length) {
                 Lampa.Select.show({
-                    title: query ? 'Найдено на NAS' : 'Jellyfin: Последние',
-                    items: data.Items.map(function(i){ return {title: i.Name, subtitle: i.ProductionYear || '', data: i} }),
-                    onSelect: function (sel) { playWithSubs(sel.data); },
-                    onBack: function () { Lampa.Controller.toggle(query ? 'full_start' : 'settings_drawer'); }
+                    title: query ? 'Поиск: ' + query : 'Jellyfin NAS',
+                    items: data.Items.map(function(i){ return { title: i.Name, subtitle: i.ProductionYear || '', data: i } }),
+                    onSelect: function (sel) { playFile(sel.data); },
+                    onBack: function () { Lampa.Controller.toggle(query ? 'full_start' : 'settings'); }
                 });
             } else {
-                Lampa.Noty.show('На Jellyfin ничего не найдено');
+                Lampa.Noty.show('Ничего не найдено');
             }
         });
     }
 
     function startPlugin() {
-        // 1. РЕГИСТРАЦИЯ В НАСТРОЙКАХ (Жесткая)
-        Lampa.Settings.register({
-            name: 'jellyfin_nas',
-            type: 'button',
-            icon: icon,
-            title: 'Jellyfin NAS',
-            description: 'Просмотр всей медиатеки с субтитрами',
-            section: 'main',
-            onPress: function () { showJellyMenu(false); }
+        // --- ВНЕДРЕНИЕ В НАСТРОЙКИ ---
+        // Создаем кнопку для главного меню настроек
+        var settings_item = $('<div class="settings-folder selector" data-component="jellyfin_nas">' +
+            '<div class="settings-folder__icon">' + icon + '</div>' +
+            '<div class="settings-folder__name">Jellyfin NAS</div>' +
+            '<div class="settings-folder__descr">Ваша медиатека с субтитрами</div>' +
+        '</div>');
+
+        settings_item.on('hover:enter', function () {
+            openJellyMenu(false);
         });
 
-        // 2. ДОБАВЛЕНИЕ КНОПКИ В КАРТОЧКУ
+        // Слушаем открытие настроек и втыкаем кнопку
+        Lampa.Listener.follow('activity', function (e) {
+            if (e.type == 'render' && e.component == 'settings') {
+                var container = e.render.find('.settings-list');
+                if (!container.find('[data-component="jellyfin_nas"]').length) {
+                    container.append(settings_item);
+                }
+            }
+        });
+
+        // --- КНОПКА В КАРТОЧКЕ ---
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
-                var container = e.object.activity.render().find('.view--torrent');
-                if (!container.length) container = e.object.activity.render().find('.full-start__buttons');
+                var btn = $('<div class="full-start__button selector view--online jelly-nas-btn">' + icon + '<span>Jellyfin</span></div>');
+                btn.on('hover:enter', function () {
+                    openJellyMenu(e.data.movie.title || e.data.movie.name);
+                });
 
-                if (container.length && !e.object.activity.render().find('.jelly-nas-btn').length) {
-                    var btn = $('<div class="full-start__button selector view--online jelly-nas-btn">' + icon + '<span>Jellyfin</span></div>');
-                    btn.on('hover:enter', function () { 
-                        showJellyMenu(e.data.movie.title || e.data.movie.name); 
-                    });
-                    container.after(btn);
-                    // Переинициализация фокуса для пульта
-                    Lampa.Controller.enable('full_start');
-                }
+                var container = e.object.activity.render().find('.view--torrent');
+                if (container.length) container.after(btn);
+                else e.object.activity.render().find('.full-start__buttons').append(btn);
+                
+                Lampa.Controller.enable('full_start');
             }
         });
     }
