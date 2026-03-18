a10cadccd4177d00dec192f360d0a11aee7b902e
cheshirskij <116010691+cheshirskij@users.noreply.github.com>
2026-03-18 14:00:47 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    // ОБНОВЛЕННЫЕ ДАННЫЕ
+    // Твои новые настройки
     var nas_host = 'http://178.234.15.238:8096';
     var nas_key  = 'b4659bb0cc0c476bb7bf3113fef553f9';
 
@@ -9,18 +9,16 @@
         var title = movie.title || movie.name;
         var extId = movie.imdb_id || movie.tmdb_id || "";
         
-        // 1. Пробуем найти по жесткому ID
         var url = nas_host + '/Items?api_key=' + nas_key + '&Recursive=true&Fields=PrimaryImageAspectRatio&AnyId=' + extId;
 
         $.ajax({
             url: url,
             method: 'GET',
-            timeout: 8000, // Увеличил таймаут для внешнего IP
+            timeout: 8000,
             success: function (data) {
                 if (data && data.Items && data.Items.length > 0) {
                     callback(data.Items);
                 } else {
-                    // 2. Если не нашли - ищем по названию
                     var sUrl = nas_host + '/Items?api_key=' + nas_key + '&searchTerm=' + encodeURIComponent(title) + '&IncludeItemTypes=Movie,Episode&Recursive=true&Limit=10';
                     $.ajax({
                         url: sUrl,
@@ -31,8 +29,8 @@
                 }
             },
             error: function (xhr) {
-                if(xhr.status === 401) Lampa.Noty.show('Jellyfin: Ошибка авторизации (401)');
-                else Lampa.Noty.show('Jellyfin: Сервер недоступен по внешнему IP');
+                if(xhr.status === 401) Lampa.Noty.show('Jellyfin: Ошибка ключа (401)');
+                else Lampa.Noty.show('Jellyfin: Сервер недоступен');
                 callback([]);
             }
         });
@@ -43,8 +41,8 @@
         var files = new Lampa.Explorer({title: 'Jellyfin NAS'});
         
         items.forEach(function (item) {
-            // Ручная отрисовка строки (чтобы не было ошибки Template not found)
-            var card = $('<div class="explorer-file selector"><div class="explorer-file__name">' + item.Name + '</div><div class="explorer-file__info">' + (item.ProductionYear || 'Jellyfin') + '</div></div>');
+            // Рисуем список без зеленой ошибки Template not found
+            var card = $('<div class="explorer-file selector"><div class="explorer-file__name">' + item.Name + '</div><div class="explorer-file__info">' + (item.ProductionYear || '') + '</div></div>');
 
             card.on('hover:enter', function () {
                 var vUrl = nas_host + '/Videos/' + item.Id + '/stream.mp4?api_key=' + nas_key + '&static=true';
@@ -64,18 +62,21 @@
     function startPlugin() {
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
-                var btn = $('<div class="full-start__button selector"><span>Jellyfin NAS</span></div>');
+                // Твоя оригинальная кнопка с классом view--online
+                var btn = $('<div class="full-start__button selector view--online"><span>Jellyfin NAS</span></div>');
                 
                 btn.on('hover:enter', function () {
-                    Lampa.Noty.show('Связь с сервером...');
+                    Lampa.Noty.show('Ищу на сервере...');
                     searchInJellyfin(e.data.movie, function(found) {
                         if (found.length > 0) showItems(found);
                         else Lampa.Noty.show('На NAS ничего не найдено');
                     });
                 });
 
-                var container = e.object.activity.render().find('.full-start__buttons');
-                if (container.length) container.append(btn);
+                // Твой оригинальный метод вставки (после Торрентов)
+                var container = e.object.activity.render().find('.view--torrent');
+                if (container.length) container.after(btn);
+                else e.object.activity.render().find('.full-start__buttons').append(btn);
             }
         });
     }
